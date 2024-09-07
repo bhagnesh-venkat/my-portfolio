12fa16e89c9cfeed664f8195413f2490368be343
KONA VENKATA SAI LAKSHMI <venkatasailakshmi7@gmail.com>
2024-09-07 16:54:36 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,58 @@
+import project1 from "../assets/project1.jpg";
+import project2 from "../assets/project2.png";
+import project3 from "../assets/project3.png";
+import project4 from "../assets/project4.jpg";
+
+
+export const HERO_CONTENT = `I am a passionate MEAN stack developer with hands-on experience in building an e-commerce website at Infosys. After refining my skills in React.js, I now specialize in creating dynamic web applications and have strong programming knowledge in C++, Python, and Java.`;
+
+export const ABOUT_TEXT = `I am a 2022 graduate with 2.5 years of hands-on experience as a MEAN stack developer. During my time at Infosys, I completed an e-commerce website project and later upskilled by diving into React.js. My recent projects include a React social media application and a personal portfolio website. I have strong programming skills in C++, Python, and Java, and I am eager to contribute to innovative projects and grow as a full-stack developer.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2022 - 2023",
+    role: "MEAN Stack Developer",
+    company: "Infosys",
+    description: `As a trained MEAN stack developer from Infosys, I successfully completed an e-commerce website project, demonstrating proficiency in MongoDB, Express.js, Angular, and Node.js, with a focus on creating dynamic, scalable web applications.`,
+    technologies: ["HTML", "CSS", "Angular", "Node.js", "Express.js", "MongoDB"],
+  },
+
+];
+
+export const PROJECTS = [
+  {
+    title: "React Social Media Application",
+    image: "project1",
+    description:
+      "A full-featured social media platform built using the MERN stack, allowing users to interact in real-time through posts, comments, and likes. Integrated secure authentication and dynamic user interface for seamless experience.",
+    technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "JWT", "Axios"]
+  },
+  {
+    title: "E-Commerce Website",
+    image: "project2",
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MySql"],
+  },
+  {
+    title: "Portfolio Website",
+    image: "project3",
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Tailwind"],
+  },
+  {
+    title: "Anomaly Detection System",
+    image: "project4",
+    description:
+      "Developed an anomaly detection system using Z-score analysis to monitor fridge door activity without additional hardware. The system identifies unusual patterns when the door is opened, ensuring reliable performance with minimal setup.",
+    technologies: ["Python", "Z-score analysis", "Data Science", "Machine Learning"]
+  }
+  
+];
+
+export const CONTACT = {
+  // address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+91 8374804272",
+  email: "konavenkatabhagnesh@gmail.com",
+};
